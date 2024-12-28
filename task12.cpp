f62a39964bf157e26ef4a18bbf97f8c087ce9362
VAlex04 <v.alex.v.04@gmail.com>
2024-12-28 19:39:39 +0700

    Update task12.cpp

--- task12.cpp
+++ task12.cpp
@@ -6,14 +6,26 @@
 #include <TMath.h>
 #include <TStyle.h>
 
-// Определение функции фитинга
+// ----------------------------------------------------------------------------
+// Функция для фитинга: Метод "P" с экспоненциальным спадом и гауссианом
+// ----------------------------------------------------------------------------
 double fitFunction(double *x, double *par) {
-    double poissonPart = par[0] * TMath::Poisson(x[0], par[1]);
-    double breitWignerPart = par[4] * TMath::BreitWigner(x[0], par[2], par[3]);
-    return poissonPart + breitWignerPart;
+    // Экспоненциальный спад
+    double expDecay = par[0] * TMath::Exp(-par[1] * x[0]);
+
+    // Гауссиан с заданным средним и sigma
+    double gaussian = par[2] * TMath::Gaus(x[0], 5.0, 1.0, true); // Среднее фиксировано на 5.0, sigma фиксирована на 1.0
+
+    // Линейный хвост
+    double linearTail = par[3] * (x[0] - 5.0); // Линейная коррекция с учетом смещения
+
+    // Итоговая модель
+    return expDecay + gaussian + linearTail;
 }
 
+// ----------------------------------------------------------------------------
 // Функция для заполнения гистограммы
+// ----------------------------------------------------------------------------
 void FillHistogram(TH1D *hist, const std::string &filename) {
     std::ifstream inFile(filename);
     if (!inFile.is_open()) {
@@ -28,54 +40,56 @@ void FillHistogram(TH1D *hist, const std::string &filename) {
     inFile.close();
 }
 
-// Основная функция
+// ----------------------------------------------------------------------------
+// Основная функция для выполнения задачи 12
+// ----------------------------------------------------------------------------
 void task12() {
-    // Настройка стиля
+    // Настройка стиля ROOT
     gStyle->SetOptFit(1111);
     gStyle->SetOptStat(1111);
 
     // Создание гистограммы
     TH1D *hist = new TH1D("hist", "Distribution data from task10Nov.dat;mm;yields / 0.1 mm", 100, 0, 10);
+    hist->Sumw2();
 
-    // Заполнение гистограммы
+    // Заполнение гистограммы данными из файла
     FillHistogram(hist, "task10Nov.dat");
 
-    // Создание функции фитинга
-    TF1 *fitFunc = new TF1("fitFunc", fitFunction, 0, 10, 5);
+    // Создание функции фитирования
+    TF1 *fitFunc = new TF1("fitFunc", fitFunction, 0, 10, 4);
 
-    // Установка параметров
-    fitFunc->SetParameter(0, 30);  // Poisson scale
-    fitFunc->SetParameter(1, 0.2); // Poisson mean
-    fitFunc->SetParameter(2, 5.4); // Breit Wigner mean
-    fitFunc->SetParameter(3, 4.0); // Breit Wigner width
-    fitFunc->SetParameter(4, 100); // Scale for Breit-Wigner
+    // Установка начальных значений параметров
+    fitFunc->SetParameter(0, 28);   // Амплитуда экспоненты
+    fitFunc->SetParameter(1, 0.65);  // Коэффициент экспоненты
+    fitFunc->SetParameter(2, 54.0); // Амплитуда гауссиана
+    fitFunc->SetParameter(3, 1.0);  // Наклон линейного хвоста
 
     // Установка ограничений параметров
-    fitFunc->SetParLimits(0, 10, hist->GetSum()); // Poisson scale
-    fitFunc->SetParLimits(1, 0.1, 0.3);           // Poisson mean
-    fitFunc->SetParLimits(2, 5.0, 5.6);           // Breit Wigner mean
-    fitFunc->SetParLimits(3, 3.5, 4.5);           // Breit Wigner width
-    fitFunc->SetParLimits(4, 50, 200);            // Scale for Breit-Wigner
+    fitFunc->SetParLimits(0, 25, 30);
+    fitFunc->SetParLimits(1, 0.5, 0.7);
+    fitFunc->SetParLimits(2, 52.0, 56.0);
+    fitFunc->SetParLimits(3, 0.7, 1.5);
 
-    // Фитинг гистограммы
-    TFitResultPtr fitRes = hist->Fit(fitFunc, "SME");
+    // Выполнение фитирования методом "P"
+    TFitResultPtr fitRes = hist->Fit(fitFunc, "PS");
 
-    // Создание холста для отображения
+    // Создание холста для отображения результата
     TCanvas *c1 = new TCanvas("c1", "Diagnostic Fit", 1200, 600);
     hist->SetLineColor(kBlue + 2);
     hist->SetLineWidth(2);
     hist->Draw("HIST");
 
+    // Добавление функции фита на график
     fitFunc->SetLineColor(kRed);
     fitFunc->SetLineWidth(2);
     fitFunc->Draw("SAME");
 
     // Сохранение графика
-    c1->SaveAs("diagnostic_fit.png");
+    c1->SaveAs("task12_fit_result_P.png");
 
-    // Вывод результатов в консоль
+    // Вывод параметров в консоль
     std::cout << "Chi2/NDF = " << fitRes->Chi2() / fitRes->Ndf() << std::endl;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 4; ++i) {
         std::cout << "Parameter " << i << ": " << fitFunc->GetParameter(i)
                   << " +/- " << fitFunc->GetParError(i) << std::endl;
     }
